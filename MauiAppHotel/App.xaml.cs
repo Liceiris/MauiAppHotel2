a96dd59ad96259840524177c08fbcfff337ff223
alice irs <aliceiris1315@gmail.com>
2025-05-23 10:40:40 -0300

    Atualizações

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,44 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
-        public App()
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+
+           new Quarto()
+           {
+            Descricao = "Suíte super Luxo",
+            ValorDiariaAdulto = 110.0,
+            ValorDiariaCrianca = 55.0
+            }, 
+            
+            new Quarto()
+            {
+             Descricao = "Suíte Luxo",
+             ValorDiariaAdulto = 90.0,
+             ValorDiariaCrianca = 45.0
+            },
+
+            new Quarto()
+            {
+             Descricao = "Suíte Simples",
+             ValorDiariaAdulto = 70.0,
+             ValorDiariaCrianca = 35.0
+            },
+
+            new Quarto()
+            {
+             Descricao = "Suíte Econômica",
+             ValorDiariaAdulto = 50.0,
+             ValorDiariaCrianca = 25.0
+            },
+        };
+
+        
+           
+    public App()
         {
             InitializeComponent();
 
